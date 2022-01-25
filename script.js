8c99135c87c3dbe75861c8183b0eee693063aa38
KalenShamy <87545752+KalenShamy@users.noreply.github.com>
2022-01-24 22:22:48 -0500

    set up board divs and spacing

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+/* Created by Kalen Shamy */
 
+function updateSize() {
+  width = outerWidth;
+  height = outerHeight;
+  if (innerWidth < width) width = innerWidth;
+  if (innerHeight < height) height = innerHeight;
+  if (width <= height) {
+    document.getElementById("board").style.width = width*0.85 + "px";
+    document.getElementById("board").style.height = width*0.75 + "px";
+  } else {
+    document.getElementById("board").style.width = height*0.85 + "px";
+    document.getElementById("board").style.height = height*0.75 + "px";
+  }
+}
+
+window.onresize = (event) => {
+  updateSize();
+};
+
+window.onload = (event) => {
+  updateSize();
+};
